7dfeeccbd2ae784d2cec0c90396fe8f23f9428a5
ArielSurco <arielsurcoa@gmail.com>
2023-12-22 00:15:17 -0300

    feat: account domain

---
+++ src/account/Account.ts
@@ -0,0 +1,24 @@
+import { User } from '../user/User'
+
+import { AccountType } from './AccountType'
+
+interface Constructor {
+  name: string
+  type: AccountType
+  user: User
+  balance?: number
+}
+
+export class Account {
+  name: string
+  type: AccountType
+  user: User
+  balance: number
+
+  constructor({ name, type, user, balance }: Constructor) {
+    this.name = name
+    this.type = type
+    this.user = user
+    this.balance = balance ?? 0
+  }
+}
---
+++ src/account/AccountType.ts
@@ -0,0 +1,5 @@
+export enum AccountType {
+  DEBIT,
+  CREDIT,
+  CASH,
+}
