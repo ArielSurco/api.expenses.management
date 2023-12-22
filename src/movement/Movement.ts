276f87a32b5b7cfe0dd377b78df88469981123af
ArielSurco <arielsurcoa@gmail.com>
2023-12-22 00:23:11 -0300

    feat: movement domain

---
+++ src/movement/Movement.ts
@@ -0,0 +1,35 @@
+import { Account } from '../account/Account'
+import { Category } from '../category/Category'
+import { User } from '../user/User'
+
+import { MovementType } from './MovementType'
+
+interface Constructor {
+  account: Account
+  detail: string
+  user: User
+  category: Category
+  date: Date
+  type: MovementType
+  amount: number
+}
+
+export class Movement {
+  account: Account
+  detail: string
+  user: User
+  category: Category
+  date: Date
+  type: MovementType
+  amount: number
+
+  constructor({ account, detail, user, category, date, type, amount }: Constructor) {
+    this.account = account
+    this.detail = detail
+    this.user = user
+    this.category = category
+    this.date = date
+    this.type = type
+    this.amount = amount
+  }
+}
---
+++ src/movement/MovementType.ts
@@ -0,0 +1,5 @@
+export enum MovementType {
+  INCOME,
+  EXPENSE,
+  TRANSFER,
+}
